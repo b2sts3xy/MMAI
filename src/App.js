9691d5bb7bb389b8c990637f9afcaafcff672dca
KyuHyun <soloist132@gmail.com>
2022-11-16 21:41:49 +0900

    골격

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Footer from './components/Footer';
+import Headers from './components/Headers';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>MMAI 헤더</div>
+      <Headers/>
+      <div>사진</div>
+      <div>설명</div>
+      <div>MMAI Solution</div>
+      <div>MMAI Gallery</div>
+      <div>MMAI White Papers</div>
+      <div>파트너 십 배너</div>
+      <div>푸터</div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Headers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Headers = () => {
+    return (
+        <div>
+            <div>MMAI</div>
+            <div>메뉴</div>
+            <div>링크</div>
+        </div>
+    );
+};
+
+export default Headers;

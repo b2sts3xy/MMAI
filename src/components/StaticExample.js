21e354e72d8a7a5dde001368b59d66821fe38bd5
KyuHyun <soloist132@gmail.com>
2022-11-17 19:25:05 +0900

    최종 마무리인가?

--- src/components/StaticExample.js
+++ src/components/StaticExample.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/Modal.css'
-
 
 function StaticExample() {
 
@@ -11,125 +10,130 @@ function StaticExample() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    return(
+    return (
         <div>
             <div className='buyBtn' onClick={handleShow}>Buy MMAI NOW!</div>
-              <Modal show={show} onHide={handleClose} size='xl'>
-<<<<<<< HEAD
+            <Modal show={show} onHide={handleClose} size='xl'>
                 <div data-layer="b6781107-2437-4b8f-9953-ea2f863db2d8" class="x1">
-            <div data-layer="25a7284c-2622-4f60-9554-363f59d2829d" class="x19061">
-                <div data-layer="28701330-5049-4643-9205-521122e23d9c" class="x19057">
-                    <div data-layer="53a392eb-557a-48fb-b1f2-5aceb766b543" class="x19054">
-                        <div data-layer="ff6ac9e7-0768-4420-bfe1-3e98e250a342" class="x19053">
-                            <div
-                                data-layer="929b3452-a329-449b-b1e7-196da2cf8cdf"
-                                class="buyMmaiToken7ae4c83d"></div>
-                            <div data-layer="68bc6d1d-d32d-4810-a387-00f3925bfe3a" class="buyMmaiToken"></div>
-                        </div>
-                        <div data-layer="207afc06-2730-4397-a155-ecd7e3eb761d" class="x19052">
-                            <div data-layer="558adfe1-2a7c-4054-bf44-713b0f431ffd" class="x19044">
-                                <div data-layer="d15ca573-2e73-4c27-9f61-bdd460038dc2" class="buy">Buy $MMAI Now</div>
-                                <div
-                                    data-layer="053a437d-313b-4509-b3ad-26bfc19f75ab"
-                                    class="soonAvailableInMoreExchanges">Soon Available in More Exchanges</div>
-                            </div>
-                            <div data-layer="bdbdad8b-1965-4ebc-a6eb-1668086071e1" class="x19047">
-                                <div data-layer="2213d52e-e918-4b39-9fdb-6e712aedd52f" class="step1">Step 1</div>
-                                <div
-                                    data-layer="13d7ffff-6f51-4602-866a-99ca34f6f992"
-                                    class="copyToYourClipboardOurOfficialContractAddress">Copy to your clipboard our Official Contract Address</div>
-                                <div
-                                    data-layer="11155478-3131-40a9-b70a-2fb07b2eba42"
-                                    class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers">
-                                    <span class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-0">(Double-check Contract Address in
-                                    </span><span
-                                        class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-34">Etherscan
-                                    </span><span
-                                        class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-44">and
-                                    </span><span
-                                        class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-48">Dextools</span>
-                                    <span
-                                        class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-56">
-                                        to Avoid Scammers)</span></div>
-                                <div
-                                    data-layer="2486f01f-0d24-4748-8d5c-c64232fd79c3"
-                                    class="x0xaa0c5b3567fd1bac8a2a11eb16c3f81a49eea90f">0xaa0c5b3567fd1bac8a2a11eb16c3f81a49eea90f</div>
-                            </div>
-                            <div data-layer="c234ed39-9678-4ddd-9f6c-b1a7cae273a1" class="x19048">
-                                <div data-layer="529c563d-3154-4e1f-a73b-bf9a49d8c60e" class="step2">Step 2</div>
-                                <div
-                                    data-layer="38713847-ad2c-4be8-b71c-7439b5344676"
-                                    class="openUniswapOfficialWebsite">Open Uniswap Official Website
-                                </div>
-                                <div data-layer="818f30bb-b4b5-47a6-bf6d-4fc4ad8293fc" class="x19046">
+                    <div data-layer="25a7284c-2622-4f60-9554-363f59d2829d" class="x19061">
+                        <div data-layer="28701330-5049-4643-9205-521122e23d9c" class="x19057">
+                            <div data-layer="53a392eb-557a-48fb-b1f2-5aceb766b543" class="x19054">
+                                <div data-layer="ff6ac9e7-0768-4420-bfe1-3e98e250a342" class="x19053">
                                     <div
-                                        data-layer="a7c7977e-fccf-4599-b588-36fff4b8bc02"
-                                        class="makeSureTheWebsiteEndsInOrg">(Make sure the website ends in .org)<br/><br/>​</div>
+                                        data-layer="929b3452-a329-449b-b1e7-196da2cf8cdf"
+                                        class="buyMmaiToken7ae4c83d"></div>
+                                    <div data-layer="68bc6d1d-d32d-4810-a387-00f3925bfe3a" class="buyMmaiToken"></div>
                                 </div>
-                                <div data-layer="b4f3edf4-ed3e-4dda-ba1b-9fd4b9b6096d" class="httpsuniswaporg"><a href='https://uniswap.org/'>https://uniswap.org/</a></div>
-                            </div>
-                            <div data-layer="3a40ed9c-e8f5-4d0c-b2ee-b0678494b97c" class="x19043">
-                                <a href='https://www.bitmart.com/trade/en?symbol=MMAI_USDT&layout=basic&theme=dark'
-                                    data-layer="49721050-8c41-4d51-9cd4-f67ab2a0f742"
-                                    class="x17aaf7be2b4f4e6eBc4602489ac1451d"></a>
-                                <a href='https://www.bkex.com/trade/MMAI_USDT'
-                                    data-layer="17aea029-0553-4d05-a6bd-39ecca16a5be"
-                                    class="x8682e61f731444f8A9951ca81641c5e5"></a>
-                                <a href='https://www.lbank.info/exchange/mmai/usdt'
-                                    data-layer="cdce2237-411b-4d09-878f-0dc41166c656"
-                                    class="x0452459dAde74bd2A93eCa6f9ed091c0"></a>
-                                <a href='https://uniswap.org/'
-                                    data-layer="3e981cce-8c07-49dd-a985-2a5962c36d72"
-                                    class="f09a0df8068844d6Ab673334fc120d76"></a>
-                                <a href='https://www.probit.com/id-id/user-center/wallet/deposit/MMAI'
-                                    data-layer="0f0af0ba-c388-4dda-b87f-c7e80547b34b"
-                                    class="picsart220919213842370"></a>
-                            </div>
-                            <div data-layer="fbefe4db-f95c-48e3-a0b2-d13e8c4e02b9" class="x19049">
-                                <div
-                                    data-layer="45738234-d1cf-4570-a359-688ed5c31d96"
-                                    class="itWillOpenTheSwapWindowAndWillAutomaticallyPairToEth">It will open the swap window and will automatically pair to ETH
+                                <div data-layer="207afc06-2730-4397-a155-ecd7e3eb761d" class="x19052">
+                                    <div data-layer="558adfe1-2a7c-4054-bf44-713b0f431ffd" class="x19044">
+                                        <div data-layer="d15ca573-2e73-4c27-9f61-bdd460038dc2" class="buy">Buy $MMAI Now</div>
+                                        <div
+                                            data-layer="053a437d-313b-4509-b3ad-26bfc19f75ab"
+                                            class="soonAvailableInMoreExchanges">Soon Available in More Exchanges</div>
+                                    </div>
+                                    <div data-layer="bdbdad8b-1965-4ebc-a6eb-1668086071e1" class="x19047">
+                                        <div data-layer="2213d52e-e918-4b39-9fdb-6e712aedd52f" class="step1">Step 1</div>
+                                        <div
+                                            data-layer="13d7ffff-6f51-4602-866a-99ca34f6f992"
+                                            class="copyToYourClipboardOurOfficialContractAddress">Copy to your clipboard our Official Contract Address</div>
+                                        <div
+                                            data-layer="11155478-3131-40a9-b70a-2fb07b2eba42"
+                                            class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers">
+                                            <span class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-0">(Double-check Contract Address in
+                                            </span>
+                                            <span
+                                                class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-34">Etherscan
+                                            </span>
+                                            <span
+                                                class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-44">and
+                                            </span>
+                                            <span
+                                                class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-48">Dextools</span>
+                                            <span
+                                                class="doubleCheckContractAddressInEtherscanAndDextoolsToAvoidScammers-56">
+                                                to Avoid Scammers)</span>
+                                        </div>
+                                        <div
+                                            data-layer="2486f01f-0d24-4748-8d5c-c64232fd79c3"
+                                            class="x0xaa0c5b3567fd1bac8a2a11eb16c3f81a49eea90f">0xaa0c5b3567fd1bac8a2a11eb16c3f81a49eea90f</div>
+                                    </div>
+                                    <div data-layer="c234ed39-9678-4ddd-9f6c-b1a7cae273a1" class="x19048">
+                                        <div data-layer="529c563d-3154-4e1f-a73b-bf9a49d8c60e" class="step2">Step 2</div>
+                                        <div
+                                            data-layer="38713847-ad2c-4be8-b71c-7439b5344676"
+                                            class="openUniswapOfficialWebsite">Open Uniswap Official Website
+                                        </div>
+                                        <div data-layer="818f30bb-b4b5-47a6-bf6d-4fc4ad8293fc" class="x19046">
+                                            <div
+                                                data-layer="a7c7977e-fccf-4599-b588-36fff4b8bc02"
+                                                class="makeSureTheWebsiteEndsInOrg">(Make sure the website ends in .org)<br/><br/>​</div>
+                                        </div>
+                                        <div data-layer="b4f3edf4-ed3e-4dda-ba1b-9fd4b9b6096d" class="httpsuniswaporg">
+                                            <a href='https://uniswap.org/'>https://uniswap.org/</a>
+                                        </div>
+                                    </div>
+                                    <div data-layer="3a40ed9c-e8f5-4d0c-b2ee-b0678494b97c" class="x19043">
+                                        <a
+                                            href='https://www.bitmart.com/trade/en?symbol=MMAI_USDT&layout=basic&theme=dark'
+                                            data-layer="49721050-8c41-4d51-9cd4-f67ab2a0f742"
+                                            class="x17aaf7be2b4f4e6eBc4602489ac1451d"></a>
+                                        <a
+                                            href='https://www.bkex.com/trade/MMAI_USDT'
+                                            data-layer="17aea029-0553-4d05-a6bd-39ecca16a5be"
+                                            class="x8682e61f731444f8A9951ca81641c5e5"></a>
+                                        <a
+                                            href='https://www.lbank.info/exchange/mmai/usdt'
+                                            data-layer="cdce2237-411b-4d09-878f-0dc41166c656"
+                                            class="x0452459dAde74bd2A93eCa6f9ed091c0"></a>
+                                        <a
+                                            href='https://uniswap.org/'
+                                            data-layer="3e981cce-8c07-49dd-a985-2a5962c36d72"
+                                            class="f09a0df8068844d6Ab673334fc120d76"></a>
+                                        <a
+                                            href='https://www.probit.com/id-id/user-center/wallet/deposit/MMAI'
+                                            data-layer="0f0af0ba-c388-4dda-b87f-c7e80547b34b"
+                                            class="picsart220919213842370" ></a>
+                                    </div>
+                                    <div data-layer="fbefe4db-f95c-48e3-a0b2-d13e8c4e02b9" class="x19049">
+                                        <div
+                                            data-layer="45738234-d1cf-4570-a359-688ed5c31d96"
+                                            class="itWillOpenTheSwapWindowAndWillAutomaticallyPairToEth">It will open the swap window and will automatically pair to ETH
+                                        </div>
+                                        <div data-layer="96f79afc-23a1-45fa-bbcc-e542ccbfe50d" class="step3">
+                                            Step 3</div>
+                                        <div
+                                            data-layer="d7955eb8-36e7-4374-ad74-a02a6f274537"
+                                            class="launchTheUniswapAppByClickingTheButtonOnTheRightUpperCorner">Launch the Uniswap app by clicking the Button on the Right upper corner.</div>
+                                        <div data-layer="64b0aba5-a61b-4bd0-8ca9-f8c98ba355af" class="uniswapProcess3"></div>
+                                        <div data-layer="41c4b517-3851-4e36-aeb2-dc332ce4c379" class="uniswapProcess2"></div>
+                                    </div>
+                                    <div data-layer="0537a4da-823d-498c-8962-e2f3d40cf5f4" class="x19050">
+                                        <div data-layer="8a267281-3161-4039-9e94-0a1c88da4dc4" class="step4">Step 4</div>
+                                        <div
+                                            data-layer="a26aeec5-d992-48b0-97f3-307ffe4c14df"
+                                            class="clickSelectTokenAndPasteTheOfficialMmaiContractAddressitWillAutomaticallyLoadTheTokenInformation">Click "Select Token" and Paste the Official MMAI contract address<br/><br/>it will automatically load the token information.
+                                        </div>
+                                        <div data-layer="3f3d5a97-f5a6-4a1a-b554-4f7670a9d430" class="uniswapProcess4"></div>
+                                    </div>
+                                    <div data-layer="5daca6df-f294-4e6b-a1cb-fcbd5b743ee0" class="x19051">
+                                        <div
+                                            data-layer="2aac5d0e-1cff-4ada-87a2-d9fad69c0dc0"
+                                            class="theFinalStepIsToSetTheAmountToBuyconnectYourCryptoWalletToContinueWithTheTransaction">The final step is to set the amount to buy<br/><br/>connect your Crypto wallet to continue with the transaction.</div>
+                                        <div data-layer="85661bd1-9ca1-4b21-8677-6692a149a692" class="step5">Step 5</div>
+                                    </div>
                                 </div>
-                                <div data-layer="96f79afc-23a1-45fa-bbcc-e542ccbfe50d" class="step3">
-                                    Step 3</div>
-                                <div
-                                    data-layer="d7955eb8-36e7-4374-ad74-a02a6f274537"
-                                    class="launchTheUniswapAppByClickingTheButtonOnTheRightUpperCorner">Launch the Uniswap app by clicking the Button on the Right upper corner.</div>
-                                <div data-layer="64b0aba5-a61b-4bd0-8ca9-f8c98ba355af" class="uniswapProcess3"></div>
-                                <div data-layer="41c4b517-3851-4e36-aeb2-dc332ce4c379" class="uniswapProcess2"></div>
                             </div>
-                            <div data-layer="0537a4da-823d-498c-8962-e2f3d40cf5f4" class="x19050">
-                                <div data-layer="8a267281-3161-4039-9e94-0a1c88da4dc4" class="step4">Step 4</div>
-                                <div
-                                    data-layer="a26aeec5-d992-48b0-97f3-307ffe4c14df"
-                                    class="clickSelectTokenAndPasteTheOfficialMmaiContractAddressitWillAutomaticallyLoadTheTokenInformation">Click "Select Token" and Paste the Official MMAI contract address<br/><br/>it will automatically load the token information.
-                                </div>
-                                <div data-layer="3f3d5a97-f5a6-4a1a-b554-4f7670a9d430" class="uniswapProcess4"></div>
-                            </div>
-                            <div data-layer="5daca6df-f294-4e6b-a1cb-fcbd5b743ee0" class="x19051">
-                                <div
-                                    data-layer="2aac5d0e-1cff-4ada-87a2-d9fad69c0dc0"
-                                    class="theFinalStepIsToSetTheAmountToBuyconnectYourCryptoWalletToContinueWithTheTransaction">The final step is to set the amount to buy<br/><br/>connect your Crypto wallet to continue with the transaction.</div>
-                                <div data-layer="85661bd1-9ca1-4b21-8677-6692a149a692" class="step5">Step 5</div>
-                            </div>
+                        </div>
+                        <div data-layer="c1426dbc-964d-4b84-a487-fc33b0c4a6f9" class="close9efd7753">
+                            <svg
+                                data-layer="e033cabb-9966-4ea5-a62a-fac1c32e3ece"
+                                preserveaspectratio="none"
+                                viewbox="0 0 30 30"
+                                class="close"></svg>
                         </div>
                     </div>
                 </div>
-                <div data-layer="c1426dbc-964d-4b84-a487-fc33b0c4a6f9" class="close9efd7753">
-                    <svg
-                        data-layer="e033cabb-9966-4ea5-a62a-fac1c32e3ece"
-                        preserveaspectratio="none"
-                        viewbox="0 0 30 30"
-                        class="close"></svg>
-                </div>
-            </div>
-        </div>
-=======
-                <div>
-                  하이
-                </div>
->>>>>>> 219129f921610e5d6fb5d62ec165d4ea3d938353
-              </Modal>
+            </Modal>
         </div>
     )
 }

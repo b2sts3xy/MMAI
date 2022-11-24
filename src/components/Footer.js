b2cc6bdacbaaffd037ce93106675afb71db9c04e
JaeHyunGround <gothddlek@naver.com>
2022-11-25 01:03:55 +0900

    revise Web#3 footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,20 @@ import React from 'react';
 import '../style/Footer.css';
 
 const Footer = () => {
+
+    const reload = () => {
+        window.location.replace('/');
+    }
+    
     return (
         <section>
             <div className='Footer'>
                 <div className='firstline'>
-                    <div className='mmaititle'>MMAI</div>
+                    <div className='mmaititle'><spen onClick={reload} className="footer_mmai">MMAI</spen></div>
                     <div className='mmaisns'>
-                        <span className="twitter"><a className='sns' href='https://twitter.com/metamonkey_ai'>twitter</a></span>
-                        <span className="telegram"><a className='sns' href='https://t.me/MMAI_Official'>telegram</a></span>
-                        <span className="Youtube"><a  className='sns' href='https://www.youtube.com/channel/UCp0Bou6D571oZ_LTeUBd0EQ'>Youtube</a></span> 
+                        <div className="twitter"><a className='sns' href='https://twitter.com/metamonkey_ai'>twitter</a></div>
+                        <div className="telegram"><a className='sns' href='https://t.me/MMAI_Official'>telegram</a></div>
+                        <div className="Youtube"><a  className='sns' href='https://www.youtube.com/channel/UCp0Bou6D571oZ_LTeUBd0EQ'>Youtube</a></div> 
                     </div>
                 </div>
                 <div className='secondline'>Ⓒ 2022, BY METAMONKY AI</div>

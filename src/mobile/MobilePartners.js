a1e2c8e91d1c7ab2b3bceddf994d364e6537e5c8
JaeHyunGround <gothddlek@naver.com>
2022-12-03 22:57:29 +0900

    mobile ing

--- src/mobile/MobilePartners.js
+++ src/mobile/MobilePartners.js
@@ -3,9 +3,23 @@ import '../mobileStyle/MobilePartners.css'
 
 const MobilePartners = () => {
     return (
-        <div>
-            <p>partners</p>
-        </div>
+        <section className='mobilePartnersContainer'>
+          <div className='mobilePartnersTitle'>AI Technology Partners</div>  
+          <div className='mobilrPartners'>
+            <div className='mobilePartnersBox'>
+                <a href='http://creativia.tech/'><img src='./imgPartners/part1.png' alt='mobile partners1' /></a>
+            </div>
+            <div className='mobilePartnersBox'>
+                <a href='https://www.ictcrc.org/'><img src='./imgPartners/part2.png 'alt='mobile partners2' /></a>
+            </div>
+            <div className='mobilePartnersBox'>
+                <a href='https://www.nslab.tech/'><img src='./imgPartners/part3.png' alt='mobile partners3' /></a>
+            </div>
+            <div className='mobilePartnersBox'>
+                <a href='https://cognitgo.ai/'><img src='./imgPartners/part4.png' alt='mobile partners4' /></a>
+            </div>
+          </div>
+        </section>
     );
 };
 
--- src/mobile/MobileWhitePapers.js
+++ src/mobile/MobileWhitePapers.js
@@ -3,9 +3,50 @@ import '../mobileStyle/MobileWhitePapers.css'
 
 const MobileWhitePapers = () => {
     return (
-        <div>
-            <p>논문</p>
-        </div>
+        <section className='mobileWhitePapersContainer'>
+            <div className='mobileWhitePapersTitle'>MMAI White Papers</div>
+            <div className='mobileWhitePapersText'>MMAI core belief is Research and then Development is more important than people think. R&D is the key component of innovation that will drive the broader adoption of Web3 technologies. The research labs are what drives our ability to obtain new knowledge that is placing MMAI tech on the cutting edge of tomorrow’s internet</div>
+
+            <div className='mobileBlockChain'>
+                <div className='mobileBlockChainTitle'>Blockchain White Papers</div>
+                <div className='mobilePapers'>
+                    <a href='https://doi.org/10.1049/cmu2.12465'><img className='mobilePapersimg' src='./imgWhitePaper/1-1.png'></img></a>
+                    <div className='mobilePaperMiniTitle'>Smart auto mining (SAM) for industrial IoT Blockchain Network</div>
+                    <div id="mobileTHIHIS">THISIS</div>
+                </div>
+                <div className='mobilePapers'>
+                    <a href='https://doi.org/10.1016/j.icte.2021.08.004'><img className='mobilePapersimg' src='./imgWhitePaper/1-2.png'></img></a>
+                    <div className='mobilePaperMiniTitle'>Blockchain side Implementation : <br/>An offline Transaction Architecture</div>
+                    <div id="mobileTHIHIS">THISIS</div>
+                </div>
+            </div>
+            <div className='mobileMeaverse'>
+                <div className='mobileMetaverseTitle'>Metaverse White Papers</div>
+                <div className='mobilePapers'>
+                    <a href='https://doi.org/10.1049/itr2.12252'><img className='mobilePapersimg' src='./imgWhitePaper/2-1.png'></img></a>
+                    <div className='mobilePaperMiniTitle'>Intelligent <br/>Transportation Systems</div>
+                    <div id="mobileTHIHIS">THISIS</div>
+                </div>
+                <div className='mobilePapers'>
+                    <a href='https://doi.org/10.48550/arXiv.2202.10336'><img className='mobilePapersimg' src='./imgWhitePaper/2-2.png'></img></a>
+                    <div className='mobilePaperMiniTitle'>Artificial <br />Intelligence</div>
+                    <div id="mobileTHIHIS">THISIS</div>
+                </div>
+            </div>
+            <div className='mobileAI'>
+                <div className='mobileAITitle'>AI & Deep Learning White Papers</div>
+                <div className='mobilePapers'>
+                    <a href='https://doi.org/10.1109/JIOT.2022.3152929'><img className='mobilePapersimg' src='./imgWhitePaper/3-1.png'></img></a>
+                    <div className='mobilePaperMiniTitle'>Low-power <br />and Lossy</div>
+                    <div id="mobileTHIHIS">THISIS</div>
+                </div>
+                <div className='mobilePapers'>
+                    <a href='https://doi.org/10.1109/JIOT.2020.3013272'><img className='mobilePapersimg' src='./imgWhitePaper/3-2.png'></img></a>
+                    <div className='mobilePaperMiniTitle'>Smart <br />Health</div>
+                    <div id="mobileTHIHIS">THISIS</div>
+                </div>
+            </div>
+        </section>
     );
 };
 

b36e563924fcb5f28acc2339031b38eabd208efb
ninano123 <dlwogus1707@naver.com>
2022-12-16 09:21:05 +0900

    add mmai blog link

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -18,10 +18,10 @@ const Headers = ({onSolutionClick,onGalleryClick, onPartnerClick}) => {
             </div>
             <div className='header_center'>
                 <spen className='header_categori1'>Home</spen>
-                <spen onClick={onSolutionClick} className='header_categori2'>MMAI SOLUTION</spen>
+                <spen onClick={onSolutionClick} className='header_categori2'>MMAI SOLUTIONS</spen>
                 <spen onClick={onGalleryClick} className='header_categori2'>MMAI GALLERY</spen>
-                <spen onClick={onPartnerClick} className='header_categori2'>Partners</spen>
-                <spen onClick={onBlogClick} className='header_categori2'>MMAI BLOG</spen>
+                <spen onClick={onPartnerClick} className='header_categori2'>PARTNERS</spen>
+                <a href='https://blog.mmaipure.com/' className='header_categori2'>MMAI BLOG</a>
             </div>
             <div className='header_right'>
                 <a href='https://twitter.com/metamonkey_ai' className='header_sns'>twitter</a>

4e314986839a187c065f59ba0ef484aafc40f9c7
minne22 <tdj06104@naver.com>
2023-01-16 10:10:03 +0900

    Appjs change

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import React, { useRef, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
+import Footer from './components/Footer';
+import Partners from './components/Partners';
+import Whitepapers from './components/Whitepapers';
+import Solution from './components/Solution';
+import Main from './components/Main';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/style/Gallery.css';
-import MobileMain from './mobile/MobileMain';
-import MoblieWorld from './mobile/MobileWorld';
-import MoblieWallet from './mobile/MobileWallet';
-import MobileFunc from './mobile/MobileFunc';
-import MoblieToken from './mobile/MobileToken';
-import MobileWhitePapers from './mobile/MobileWhitePapers';
-import MobilePartners from './mobile/MobilePartners';
-import MobileFooter from './mobile/MobileFooter';
+import AutoPlaySecond from './components/AutoPlaySecond';
+import AutoPlay from './components/AutoPlay'
 import MainPage from './components/MainPage';
 import Guide from './components/Guide';
-import MarketPlacePopUp from './components/MarketPlacePopUp';
+import MobileMainPage from './mobile/MobileMainPage';
 
 function App() {
 
-  // useEffect(() => {
-  //   window.open("https://www.naver.com/", "_blank", 'width=900,height=900');
-  // },[]);
+  const solutionRef = useRef(null);
+  const galleryRef = useRef(null);
+  const partnerRef = useRef(null);
+
+  const onSolutionClick = () => {
+    solutionRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  const onGalleryClick = () => {
+    galleryRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  const onPartnerClick = () => {
+    partnerRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const [rendNum, setRendNum] = useState(false);
+
 
   return (
     <section className="App">
       <div className='pc'>
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route exact path="/" element={<MainPage/>}></Route>
             <Route exact path="/Guide" element={<Guide/>}></Route>
-            {/* <Route exact path="/mmaimarketplace" element={<MarketPlacePopUp />}></Route> */}
           </Routes>
-        </BrowserRouter>
+        </Router>
       </div>
 
 
       <div className='mobile'>
-        <MobileMain />
-        <MoblieWorld />
-        <MoblieWallet />
-        <MobileFunc />
-        <MoblieToken />
-        <MobileWhitePapers />
-        <MobilePartners />
-        <MobileFooter />
+        <Router>
+          <Routes>
+            <Route path='/' element={<MobileMainPage />} />
+            <Route path='/Guide' element={<Guide />}></Route>
+          </Routes>
+        </Router>
       </div>
     </section>
   );

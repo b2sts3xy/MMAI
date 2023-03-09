511293f7182942bb7ae8511f2f506142cea6ce96
JaeHyunGround <gothddlek@naver.com>
2023-03-09 18:02:56 +0900

    fix : more info

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -108,19 +108,6 @@ const MoreInfo = ({whatInfo, setInfoState}) => {
         setInfoState(false);
     }
 
-    useEffect(() => {
-        document.body.style.cssText = `
-          position: fixed; 
-          top: -${window.scrollY}px;
-          overflow-y: scroll;
-          width: 100%;`;
-        return () => {
-          const scrollY = document.body.style.top;
-          document.body.style.cssText = '';
-          window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-        };
-      }, []);
-
     return (
         <div className='moreinfo_out' onClick={onCloseInfo}>
             <div className='moreinfo_container'>

702e84d3ca326cc06ff5072fd23755c794f34e1a
Moyujin <scab12341@gmail.com>
2023-04-21 18:53:10 +0900

    feat: 게시판 추가

--- src/components/board/Write.js
+++ src/components/board/Write.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Editor from "./Editor";
+import { Button } from "antd";
+import "../../style/board/css/Write.css";
 
 const Write = () => {
   const defaultString = "Hello, This is CKEditor~~";
   const [content, setContent] = useState(defaultString);
   return (
-    <div>
-      <div>Write</div>
+    <div className="WriteContainer">
       <Editor
         data={content}
         uploadFolder="Test"
@@ -17,6 +18,7 @@ const Write = () => {
           console.log({ event, editor, data });
         }}
       />
+      <Button className="writeBtn">작성하기</Button>
     </div>
   );
 };

f6e7ca5f6310499147d1a77861c0919a29ddeeb5
Moyujin <scab12341@gmail.com>
2023-05-31 15:01:28 +0900

    사이드바 제작

---
+++ src/components/navBar/SideBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../style/sideBar/css/SideBar.css";
+
+const SideBar = () => {
+  return (
+    <div className="SideBar">
+      <nav>
+        <span className="text">Pure World</span>
+        <span className="text">MMAI GAMES</span>
+        <span className="text">General</span>
+        <span className="text">News</span>
+      </nav>
+    </div>
+  );
+};
+
+export default SideBar;
